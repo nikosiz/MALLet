415bbb07de2312fbf9db0010da2d01f06e1e2748
Nikodem <moj.mail.do.kont@gmail.com>
2023-08-02 22:09:22 +0200

    Created splash screen

--- app/src/main/java/com/example/mallet/SplashScreenActivity.java
+++ app/src/main/java/com/example/mallet/SplashScreenActivity.java
@@ -1,14 +1,28 @@
 package com.example.mallet;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.content.Intent;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.mallet.MainActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_splash_screen); // Assuming you have a layout file for the splash screen
+
+        // Add a Handler to post a delayed Runnable that starts MainActivity after 5 seconds
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Start MainActivity
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                // Finish SplashScreenActivity to prevent going back to it when pressing back from MainActivity
+                finish();
+            }
+        }, 2500); // 5000 milliseconds = 5 seconds delay
     }
 }
