db2c0e51a67dd9696c2234ae2b79eac31b732582
Nikodem <moj.mail.do.kont@gmail.com>
2023-08-04 17:27:40 +0200

    Added bottom navigation bar using fragments

--- app/src/main/java/com/example/mallet/MainActivity.java
+++ app/src/main/java/com/example/mallet/MainActivity.java
@@ -1,16 +1,51 @@
 package com.example.mallet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.mallet.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setBackground(null);
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.bottom_nav_home) {
+                replaceFragment(new HomeFragment());
+            } else if (itemId == R.id.bottom_nav_library) {
+                replaceFragment(new LibraryFragment());
+            } else if (itemId == R.id.bottom_nav_add_new) {
+                replaceFragment(new AddNewFragment());
+            } else if (itemId == R.id.bottom_nav_your_library) {
+                replaceFragment(new YourFragment());
+            } else if (itemId == R.id.bottom_nav_profile) {
+                replaceFragment(new ProfileFragment());
+            }
+
+            return true;
+        });
+
     }
 
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.bottom_navigation_frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
 
 }
