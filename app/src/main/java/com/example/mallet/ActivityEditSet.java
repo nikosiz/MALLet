4f75a613cb99f54066de4d397b4b84efe6406ab3
Nikodem <moj.mail.do.kont@gmail.com>
2023-09-02 17:03:06 +0200

    Change creating sets, folders and groups. Now it is done through a dialog, not separate activities.

---
+++ app/src/main/java/com/example/mallet/ActivityEditSet.java
@@ -0,0 +1,60 @@
+package com.example.mallet;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.mallet.databinding.ActivityEditSetBinding;
+
+public class ActivityEditSet extends AppCompatActivity {
+    private ActivityEditSetBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityEditSetBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Initialize and set up the toolbar
+        setupToolbar();
+        setupClickListeners();
+    }
+
+    // Initialize and set up the toolbar with back arrow functionality.
+    private void setupToolbar() {
+        Toolbar toolbar = findViewById(R.id.create_set_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(""); // Set the title to an empty string
+
+        // Display back arrow on the toolbar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+    }
+
+    private void setupClickListeners() {
+        binding.createSetSaveBtn.setOnClickListener(v -> createSet());
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle arrow click here
+        if (item.getItemId() == android.R.id.home) {
+            // Finish this activity and return to the previous activity (if any)
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void createSet() {
+        // TODO
+        FrontendUtils.showToast(this, "Set created");
+    }
+
+
+}
--- app/src/main/java/com/example/mallet/ActivityMain.java
+++ app/src/main/java/com/example/mallet/ActivityMain.java
@@ -11,13 +11,14 @@ import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.LinearLayout;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,8 +28,13 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.mallet.databinding.ActivityMainBinding;
+import com.example.mallet.databinding.DialogCreateFolderBinding;
+import com.example.mallet.databinding.DialogCreateGroupBinding;
+import com.example.mallet.databinding.DialogCreateSetBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.Objects;
 
@@ -89,14 +95,129 @@ public class ActivityMain extends AppCompatActivity {
         LinearLayout createFolder = dialog.findViewById(R.id.add_new_create_folder);
         LinearLayout createGroup = dialog.findViewById(R.id.add_new_create_group);
 
-        createSet.setOnClickListener(view -> startNewActivity(ActivityCreateSet.class, dialog));
-        createFolder.setOnClickListener(view -> startNewActivity(ActivityCreateFolder.class, dialog));
-        createGroup.setOnClickListener(view -> startNewActivity(ActivityCreateGroup.class, dialog));
+        createSet.setOnClickListener(v -> {
+            dialog.dismiss();
+            dialogCreateNewSet();
+        });
+
+        createFolder.setOnClickListener(v -> {
+            dialog.dismiss();
+            dialogCreateNewFolder();
+        });
+
+        createGroup.setOnClickListener(v -> {
+            dialog.dismiss();
+            dialogCreateNewGroup();
+        });
 
         dialog.show();
         Objects.requireNonNull(dialog.getWindow()).setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         dialog.getWindow().setGravity(Gravity.BOTTOM);
+    }
+
+    private void dialogCreateNewSet() {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        DialogCreateSetBinding binding = DialogCreateSetBinding.inflate(LayoutInflater.from(this));
+        dialog.setContentView(binding.getRoot());
+
+        TextInputEditText setNameTIET = binding.createSetNameEt;
+        TextInputLayout descriptionTIL = binding.createSetDescriptionTil;
+        binding.createSetAddDescriptionLl.setOnClickListener(v -> FrontendUtils.showItem(descriptionTIL));
+        binding.createSetCancelBtn.setOnClickListener(v -> dialog.dismiss());
+        binding.createSetConfirmBtn.setOnClickListener(v -> {
+            TextInputEditText setNameEditText = binding.createSetNameEt;
+            String setName = Objects.requireNonNull(setNameEditText.getText()).toString();
+            TextView emptyNameError = binding.createSetEmptyNameError;
+
+            if (TextUtils.isEmpty(setName)) {
+                FrontendUtils.showItem(emptyNameError);
+            } else {
+                FrontendUtils.hideItem(emptyNameError);
+                createSetAndOpenEditActivity(dialog);
+                dialog.dismiss();
+            }
+        });
+
+        FrontendUtils.showDialog(dialog);
+    }
+
+    private void createSetAndOpenEditActivity(Dialog dialog) {
+        Intent intent = new Intent(this, ActivityEditSet.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(intent);
+        FrontendUtils.showToast(this, "Here you can add vocabulary to your new set... In the future... With backend...");
+    }
+
+    private void dialogCreateNewFolder() {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        DialogCreateFolderBinding binding = DialogCreateFolderBinding.inflate(LayoutInflater.from(this));
+        dialog.setContentView(binding.getRoot());
+
+        TextInputEditText setNameTIET = binding.createFolderNameEt;
+        TextInputLayout descriptionTIL = binding.createFolderDescriptionTil;
+
+        binding.createFolderAddDescriptionLl.setOnClickListener(v -> FrontendUtils.showItem(descriptionTIL));
+        binding.createFolderCancelBtn.setOnClickListener(v -> dialog.dismiss());
+        binding.createFolderConfirmBtn.setOnClickListener(v -> {
+            TextInputEditText setNameEditText = binding.createFolderNameEt;
+            String setName = Objects.requireNonNull(setNameEditText.getText()).toString();
+            TextView emptyNameError = binding.createFolderEmptyNameError;
+
+            if (TextUtils.isEmpty(setName)) {
+                FrontendUtils.showItem(emptyNameError);
+            } else {
+                FrontendUtils.hideItem(emptyNameError);
+                createFolderAndOpenEditActivity(dialog);
+                dialog.dismiss();
+            }
+        });
+
+        FrontendUtils.showDialog(dialog);
+    }
+
+    private void createFolderAndOpenEditActivity(Dialog dialog) {
+        Intent intent = new Intent(this, ActivityViewFolder.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(intent);
+        FrontendUtils.showToast(this, "Here you can view and manage your folder... In the future... With backend...");
+    }
+
+    private void dialogCreateNewGroup() {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        DialogCreateGroupBinding binding = DialogCreateGroupBinding.inflate(LayoutInflater.from(this));
+        dialog.setContentView(binding.getRoot());
+
+        TextInputEditText setNameTIET = binding.createGroupNameEt;
+        TextInputLayout descriptionTIL = binding.createGroupDescriptionTil;
+
+        binding.createGroupAddDescriptionLl.setOnClickListener(v -> FrontendUtils.showItem(descriptionTIL));
+        binding.createGroupCancelBtn.setOnClickListener(v -> dialog.dismiss());
+        binding.createGroupConfirmBtn.setOnClickListener(v -> {
+            TextInputEditText setNameEditText = binding.createGroupNameEt;
+            String setName = Objects.requireNonNull(setNameEditText.getText()).toString();
+            TextView emptyNameError = binding.createGroupEmptyNameError;
+
+            if (TextUtils.isEmpty(setName)) {
+                FrontendUtils.showItem(emptyNameError);
+            } else {
+                FrontendUtils.hideItem(emptyNameError);
+                createGroupAndOpenEditActivity(dialog);
+                dialog.dismiss();
+            }
+        });
+
+        FrontendUtils.showDialog(dialog);
+    }
+
+    private void createGroupAndOpenEditActivity(Dialog dialog) {
+        Intent intent = new Intent(this, ActivityViewGroup.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(intent);
+        FrontendUtils.showToast(this, "Here you can view and manage your folder... In the future... With backend...");
     }
 
     // Method to start a create activity and dismiss the dialog
@@ -134,5 +255,5 @@ public class ActivityMain extends AppCompatActivity {
         return true;
     }
 
-    
+
 }
