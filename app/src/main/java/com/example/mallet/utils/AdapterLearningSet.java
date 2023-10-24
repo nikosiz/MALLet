999110e34b25362a79d557386cc563ffb5aeb49d
Nikodem <moj.mail.do.kont@gmail.com>
2023-10-25 00:02:34 +0200

    Updated toolbar options

--- app/src/main/java/com/example/mallet/utils/AdapterLearningSet.java
+++ app/src/main/java/com/example/mallet/utils/AdapterLearningSet.java
@@ -14,12 +14,11 @@ import com.example.mallet.R;
 import java.util.List;
 
 public class AdapterLearningSet extends RecyclerView.Adapter<AdapterLearningSet.ViewHolder> {
-    ModelLearningSet binding; // This line seems to be unrelated to the adapter and can be removed.
+    ModelLearningSet binding;
     private final Context context;
     private final List<ModelLearningSet> learningSetList;
     private final AdapterLearningSet.OnLearningSetClickListener setClickListener;
 
-    // Constructor to initialize the adapter with data and click listener
     public AdapterLearningSet(Context context, List<ModelLearningSet> learningSetList, OnLearningSetClickListener setClickListener) {
         this.context = context;
         this.learningSetList = learningSetList;
@@ -29,22 +28,22 @@ public class AdapterLearningSet extends RecyclerView.Adapter<AdapterLearningSet.
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        // Inflate the learning_set_item.xml layout for each item view
         View itemView = LayoutInflater.from(context).inflate(R.layout.model_learning_set, parent, false);
         return new ViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        // Get the learning set data at the current position
         ModelLearningSet learningSet = learningSetList.get(position);
 
-        // Bind learning set data to the views within the ViewHolder
-        holder.learningSetNameTv.setText(learningSet.getName());
-        holder.learningSetCreatorTv.setText(learningSet.getCreator());
-        holder.learningSetTermsTv.setText(learningSet.getNrOfTerms() + " terms");
+        if (learningSet != null) {
+            holder.learningSetNameTv.setText(learningSet.getName());
+            holder.learningSetCreatorTv.setText(learningSet.getCreator());
+            holder.learningSetTermsTv.setText(learningSet.getNrOfTerms() + " terms");
+        } else {
+            // Handle the case when learningSet is null.
+        }
 
-        // Set a click listener for the learning set item
         holder.itemView.setOnClickListener(view -> {
             if (setClickListener != null) {
                 setClickListener.onSetClick(learningSet);
@@ -54,11 +53,9 @@ public class AdapterLearningSet extends RecyclerView.Adapter<AdapterLearningSet.
 
     @Override
     public int getItemCount() {
-        // Return the nr of learning sets in the list
         return learningSetList.size();
     }
 
-    // ViewHolder class that holds references to the views within each item view
     public static class ViewHolder extends RecyclerView.ViewHolder {
         final TextView learningSetNameTv, learningSetCreatorTv, learningSetTermsTv;
 
